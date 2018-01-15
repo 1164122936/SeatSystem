cf161acd3f4aef3fe4d6df95ea019be21d4522b3
1164122936 <1164122936@qq.com>
2018-01-15 21:40:04 +0800

    预约第四部

---
+++ Confirm.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <el-table
+    :data="tableData"
+    style=margin-top:2%;width:90%>
+    <el-table-column
+      prop="stu"
+      label="账号">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="预约日期">
+    </el-table-column>
+    <el-table-column
+      prop="time"
+      label="预约时间">
+    </el-table-column>
+    <el-table-column
+      prop="place"
+      label="阅览室">
+    </el-table-column>
+    <el-table-column
+      prop="num"
+      label="座位">
+    </el-table-column>
+  </el-table><br>
+  <el-button type="danger" @click="dialogVisible = false" style=margin-left:35% >取 消</el-button>
+  <el-button type="primary" @click="dialogVisible = false">提交</el-button>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "confirm",
+    data() {
+      return {
+        dialogVisible: false,
+        tableData: [{
+          stu:'3140602035',
+          date: '2018-01-17',
+          time: '早上',
+          place: '1楼C区',
+          num:'23'
+        }]
+
+      };
+    },
+
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ Reservation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Header />
+    <TimeReservation v-if="active === 0"/>
+    <RoomChoose v-if="active === 1"/>
+    <SeatChoose v-if="active === 2"/>
+    <Confirm v-if="active===3"/>
+  </div>
+</template>
+
+<script type="es6">
+  import Header from './Header'
+  import TimeReservation from './TimeReservation'
+  import RoomChoose from './RoomChoose'
+  import SeatChoose from './SeatChoose'
+  import Confirm from './Confirm'
+  import { mapState } from 'vuex';
+  export default {
+    name: 'Reservation',
+    components: {Header, TimeReservation,RoomChoose,SeatChoose,Confirm},
+    computed: {
+      ...mapState({
+        active: state => state.Reservation.active,
+      })
+    },
+  }
+</script>
+
+<style>
+
+</style>
